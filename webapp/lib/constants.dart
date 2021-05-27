af8770168b250c631a72883e03d0e49adb7b11bc
TImothy Odebunmi <dev.brymes@gmail.com>
2021-05-27 17:32:56 +0100

    Changes

--- webapp/lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xFFE50000);
-const secondaryColor = Color(0xFF4A5767);
-const bgColor = Color(0xFFFFFFFF);
-
-const defaultPadding = 16.0;
